03af1c11c4cebda7466308f2abab055cbd5f681f
Elisariane <elisarianeBS@gmail.com>
2025-02-20 17:58:23 -0300

    refactor: Refactor style from bootstrap to tailwindcss

--- src/app/components/landingpage/landingpage.component.ts
+++ src/app/components/landingpage/landingpage.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { SectionToolsComponent } from "./components/section-tools/section-tools.component";
 import { FooterComponent } from "./components/footer/footer.component";
@@ -8,7 +6,7 @@ import { FooterComponent } from "./components/footer/footer.component";
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, SectionToolsComponent, FooterComponent],
+  imports: [HeroComponent, SectionToolsComponent, FooterComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
